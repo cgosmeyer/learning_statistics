f0115d44a431e294bc6f8129d1824a327a1e7971
Gosmeyer <cgosmeye@gs615-crystal.local>
2018-03-20 18:07:25 -0400

    Finished multisample.py and associated pytests.:

--- stats/inferential_stats/multisample.py
+++ stats/inferential_stats/multisample.py
@@ -14,11 +14,167 @@ References:
     J.C. McGrew, Jr., A.J. Lembo, Jr., C.B. Monroe
 """
 
-class ANOVA():
+import numpy as np
+
+class ANOVA(object):
+    """ Compares three or more independent random sample means for
+    difference.
+
+    Requirements
+    ------------
+    1. Three or more (k) random samples.
+    2. Each population *normally* distributed.
+    3. Each population has *equal* variance.
+    4. Variable is measured at interval or ratio scale.
+
+    Null Hypthothesis
+    -----------------
+    mean1 = mean2 = ... = meank
+
+    Test Statistic
+    --------------
+
+        F = BetweenGroupMeanSquares / WithinGroupMeanSquares
+
+        where
+
+        BetweenGroupMeanSquares = BetweenGroupSumOfSquares / (NumberSamples - 1)
+
+        where 
+
+        BetweenGroupSumOfSquares = sum( NumberObservationsInSample_i * 
+                                       (MeanOfSample_i - OverallMean)^2 )
+
+        where
+        
+        OverallMean = sum( NumberObservationsInSample_i * MeanOfSample_i /
+                           TotalNumberObservationsInAllSamples)
+
+        and 
+
+        WithinGroupMeanSquares = WithinGroupSumOfSquares / 
+                                 (TotalNumberObservationsInAllSamples - NumberSamples)
+
+        where
+
+        WithinGroupSumOfSquares = sum( (NumberObservationsInSample_i - 1) *
+                                      VarianceSample_i^2 )      
+    """                         
+    def __init__(self, means, ns, variances):
+        """
+        Parameters
+        ----------
+        means : array
+            Mean of each sample.
+        ns : array
+            Number of observations in each sample.
+        variances : array
+            Variance of each sample.
+        """
+        self.means = np.asarray(means)
+        self.ns = np.asarray(ns)
+        self.variances = np.asarray(variances)
+        self.k = len(means)
+        self.N = sum(ns)
+        self.F = None
+        self.SS_B = self.between_group_sum_of_squares()
+        self.SS_W = self.within_group_sum_of_squares()
+        self.MS_B = self.between_group_mean_squares()
+        self.MS_W = self.within_group_mean_squares()
+
+        self.test_statistic()
+        self.test_stat = self.F
+
+    def between_group_sum_of_squares(self):
+        X_T = sum([self.ns[i]*self.means[i] for i in range(self.k)]) / float(self.N)
+        SS_B = sum([self.ns[i]*(self.means[i] - X_T)**2 for i in range(self.k)])
+        return SS_B
+
+    def within_group_sum_of_squares(self):
+        SS_W = sum( [(self.ns[i] - 1)*self.variances[i]**2 for i in range(self.k)] )
+        return SS_W
+
+    def between_group_mean_squares(self):
+        MS_B = self.SS_B / float(self.k - 1)
+        return MS_B
+
+    def within_group_mean_squares(self):
+        MS_W = self.SS_W / float(self.N - self.k)
+        return MS_W
+
+    def test_statistic(self):
+        self.F = self.MS_W / self.MS_B
+
+    def get_F(self):
+        print(self.F)
 
 
+class KruskalWallis(object):
+    """ Compares three or more independent random sample mean ranks
+    for difference.
 
-class KruskalWallis():
+    Requirements
+    ------------
+    1. Three or more (k) random samples.
+    2. Each population has an underlying continuous distribution.
+    3. Variable is measured at ordinal scale or downgraded from 
+       interval/ratio scale to ordinal.
+
+    Null Hypthothesis
+    -----------------
+    The populations from which the three or more (k) samples have been
+    drawn are all identical.
+
+    Test Statistic
+    --------------
+
+        H = { [ (12 / (N * (N + 1)) * sum(NumberObservationsInSample_i * MeanRankInSampl_i) ] -
+              3 * (N + 1) } /
+            [ 1 - sum(T) / (N^3 - N)]
+
+        where
+
+        N = TotalNumberObservationsInAllSamples
+
+        T = NumberTiedObservations^3 - NumberTiedObservations
+
+    """                         
+    def __init__(self, rs):
+        """
+        Parameters
+        ----------
+        rs : array
+            List of ranks of each sample.
+        """
+        self.rs = np.asarray(rs)
+        self.k = len(rs)
+        self.ns = np.array([len(rs[i]) for i in range(self.k)])
+        self.N = float(sum(self.ns))
+        self.H = None
+        self.sum_n_r = None
+        self.mean_rs = self.mean_rs()
+        self.T = self.tie_correction()
+        self.test_statistic()
+
+    def tie_correction(self):
+        num_tied_observations = len(self.mean_rs) - len(set(self.mean_rs))
+        T = num_tied_observations**3 - num_tied_observations
+        return T
+
+    def mean_rs(self):
+        rs = self.rs
+        mean_rs = [np.array(rs[i]).mean() for i in range(self.k)]
+        return mean_rs
+
+    def test_statistic(self):
+        N = self.N
+
+        self.sum_n_r = sum([self.ns[i]*(self.mean_rs[i])**2 for i in range(self.k)])
+
+        self.H = ( ( (12/(N*(N + 1))) * self.sum_n_r ) - 3*(N+1) ) / \
+            float(1 - (self.T/(N**3 - N)))
+
+    def get_H(self):
+        print(self.H)
 
 
-	
---
+++ tests/test_mulitsample.py
@@ -0,0 +1,91 @@
+""" Validation tests for the multi-sample classes.
+
+Author:
+    
+    C.M. Gosmeyer
+
+Date:
+
+    Mar 2018
+
+References:
+
+    "Introduction to Statistical Problem Solving in Geography", 
+    J.C. McGrew, Jr., A.J. Lembo, Jr., C.B. Monroe
+
+"""
+
+import numpy as np
+import pytest
+from stats.inferential_stats.multisample import *
+
+class TestANOVA(object):
+    """ Uses table 11.1.
+    """
+    def setup(self):
+        means = np.array([28.780, 25.914, 30.609, 25.819])
+        ns = np.array([65, 14, 90, 31])
+        variances = np.array([1.544, 4.295, 3.006, 5.104])
+        return ANOVA(means, ns, variances)
+    
+    def test_between_group_sum_of_squares(self):
+        Ftest = self.setup()
+        val = round(Ftest.SS_B, 1)
+        assert val == 682.5
+
+    def test_between_group_mean_squares(self):
+        Ftest = self.setup()
+        val = round(Ftest.MS_B, 1)
+        assert val == 227.5
+
+    def test_within_group_sum_of_squares(self):
+        Ftest = self.setup()
+        val = round(Ftest.SS_W, 0)
+        assert val == 1978.0
+
+    def test_within_group_mean_squares(self):
+        Ftest = self.setup()
+        val = round(Ftest.MS_W, 1)
+        assert val == 10.1
+
+    def test_F(self):
+        Ftest = self.setup()
+        val = round(Ftest.MS_W, 1)
+        assert val == 10.1
+
+class TestKruskalWallis(object):
+    """ Uses tables 11.3 - 11.4.
+    """
+    def setup(self):
+        rs = np.array([
+            [17, 23, 5, 6, 22, 3, 12, 15, 10],
+            [13.5, 19, 25.5, 33, 29, 21, 25.5, 27, 8, 30],
+            [11, 2, 1, 9, 4, 18, 7, 16],
+            [13.5, 24, 28, 31, 34, 32, 20]])
+        return KruskalWallis(rs)
+
+    def test_N(self):
+        Htest = self.setup()
+        val = Htest.N
+        assert val == 34       
+
+    def test_T(self):
+        Htest = self.setup()
+        val = Htest.T 
+        assert val == 0
+
+    def test_mean_rs1(self):
+        Htest = self.setup()
+        val = round(Htest.mean_rs[0], 1)
+        assert val == 12.6   
+
+    def test_sum_n_r(self):
+        Htest = self.setup()
+        val = round(Htest.sum_n_r, 1)
+        assert val == 12114.0        
+
+    def test_H(self):
+        Htest = self.setup()
+        val = round(Htest.H, 2)
+        assert val == 17.16
+
